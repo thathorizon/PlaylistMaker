ec67f327e5b53890d43c51dac20138659f298bcd
KirVar <el.vartanov@gmail.com>
2024-11-20 02:34:29 +0300

    3 homework (Sprint 9) add interface search display and functions for it

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,55 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.search_input_text)
+        val clearButton = findViewById<ImageView>(R.id.buttonClearInputText)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputEditText.hideKeyboard()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.core.content.IntentCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,32 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val shareApp = findViewById<ImageView>(R.id.shareApp)
+        shareApp.setOnClickListener {
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/profile/android-developer")
+            shareIntent.type = "text/plain"
+            startActivity(Intent.createChooser(shareIntent, "Send to:"))
+        }
+
+        val writeToSupport = findViewById<ImageView>(R.id.writeToSupport)
+        writeToSupport.setOnClickListener {
+            val subjectText = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val text = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val writeToSupportIntent = Intent(Intent.ACTION_SEND)
+            writeToSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("kirvart@yandex.ru"))
+            writeToSupportIntent.putExtra(Intent.EXTRA_SUBJECT, subjectText)
+            writeToSupportIntent.putExtra(Intent.EXTRA_TEXT, text)
+            writeToSupportIntent.type = "message/rfc822"
+            startActivity(writeToSupportIntent)
+        }
+        val userAgreement = findViewById<ImageView>(R.id.userAgreement)
+        userAgreement.setOnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer"
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW)
+            userAgreementIntent.data = Uri.parse(url)
+            startActivity(userAgreementIntent)
+        }
     }
 }
